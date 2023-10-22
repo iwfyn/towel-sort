8ef7bcb97a6eccd5e2d6de201242f03fd18ef9b4
Dzima Bykau <bykovdmitry3@gmail.com>
2023-10-22 20:25:54 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix = []) {
+    return matrix.reduce(function (sum, item, ind) {
+      item.sort((a, b) => (ind % 2 == 0 || ind == 0) ? a - b : b - a,).map(el => sum.push(el))
+      return sum;
+    }, []);
+};
